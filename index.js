fc0e364cbeb78d4afb03778ed7f1fcef31c54c5e
jkgdev8 <jkgdev8@gmail.com>
2022-05-29 22:30:50 -0700

    added mp lister

---
+++ index.js
@@ -0,0 +1,17 @@
+setInterval(function(){
+    var i =0;
+    var e = document.getElementsByClassName("i09qtzwb n7fi1qx3 b5wmifdl hzruof5a pmk7jnqg j9ispegn kr520xx4 c5ndavph art1omkt ot9fgl3s rnr61an3 s45kfl79 emlxlaya bkmhp75w spb7xbtv" );
+    e[i].click();
+    var e = document.getElementsByClassName( "oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz p7hjln8o esuyzwwr f1sip0of n00je7tq arfg74bv qs9ysxi8 k77z8yql abiwlrkh p8dawk7l lzcic4wl dwo3fsh8 rq0escxv nhd2j8a9 j83agx80 btwxx1t3 pfnyh3mw opuu4ng7 kj2yoqh6 kvgmc6g5 oygrvhab l9j0dhe7 i1ao9s8h du4w35lb bp9cbjyn cxgpxx05 dflh9lhu sj5x9vvc scb9dxdr");
+    e[i].click();
+    var e = document.getElementsByClassName( "l9j0dhe7 du4w35lb j83agx80 pfnyh3mw taijpn5t bp9cbjyn owycx6da btwxx1t3 kt9q3ron ak7q8e6j isp2s0ed ri5dt5u2 rt8b4zig n8ej3o3l agehan2d sk4xxmp2 rq0escxv d1544ag0 tw6a2znq s1i5eluu tv7at329");
+    e[i].click();
+    
+    
+    
+    
+
+},2500);
+
+// mp lister asdasdasd
+asdasds
