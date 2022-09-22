4f9bb9ae22c5a54e1c42b48b7f456b0dcaff42ec
jkgdev8 <jkgdev8@gmail.com>
2022-09-21 22:09:01 -0700

    dupe

--- index.js
+++ index.js
@@ -60,5 +60,29 @@ setInterval(function () {
 
 }, 2000);
 
+setInterval(function () {
+    var i = 0;
+    var e = document.getElementsByClassName("qi72231t n3hqoq4p r86q59rh b3qcqh3k fq87ekyn fsf7x5fv s5oniofx m8h3af8h l7ghb35v kjdc1dyq kmwttqpk cr00lzj9 rn8ck1ys s3jn8y49 f14ij5to l3ldwz01 icdlwmnq i85zmo3j qmqpeqxj e7u6y3za qwcclf47 nmlomj2f frfouenu bonavkto djs4p424 r7bn319e bdao358l alzwoclg jcxyg2ei srn514ro oxkhqvkx rl78xhln nch0832m om3e55n1 jvc6uz2b g90fjkqk nu7423ey");
+    e[i].click();
+    var e = document.getElementsByClassName("");
+    e[i].click();
+    var e = document.getElementsByClassName("");
+    e[i].click();
 
+
+
+}, 2000);
+
+setInterval(function () {
+    var i = 0;
+    var e = document.getElementsByClassName("qi72231t n3hqoq4p r86q59rh b3qcqh3k fq87ekyn fsf7x5fv s5oniofx m8h3af8h l7ghb35v kjdc1dyq kmwttqpk cr00lzj9 rn8ck1ys s3jn8y49 f14ij5to l3ldwz01 icdlwmnq i85zmo3j qmqpeqxj e7u6y3za qwcclf47 nmlomj2f frfouenu bonavkto djs4p424 r7bn319e bdao358l alzwoclg jcxyg2ei srn514ro oxkhqvkx rl78xhln nch0832m om3e55n1 jvc6uz2b g90fjkqk nu7423ey");
+    e[i].click();
+    var e = document.getElementsByClassName("");
+    e[i].click();
+    var e = document.getElementsByClassName("");
+    e[i].click();
+
+
+
+}, 2000);
 
